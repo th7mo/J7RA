a35b5749b47691c400f5fa1a2bc8355453a97a11
th7mo <dev@th7mo.com>
2023-03-07 12:23:43 +0100

    Initial commit

---
+++ src/components/Card.stories.js
@@ -0,0 +1,23 @@
+import Card from './Card.vue';
+
+export default {
+    title: 'Card',
+    component: Card,
+}
+
+const Template = (args) => ({
+    components: { Card },
+    setup() {
+        return { args };
+    },
+    template: `
+        <Card v-bind="args" />
+    `
+});
+
+export const ShortDescription = Template.bind({});
+
+ShortDescription.args = {
+    title: 'Primary',
+    subText: 'Example sub Text',
+}
---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps({
+    title: String,
+    subText: String
+})
+</script>
+
+<template>
+  <div class="card">
+    <h4 class="title">{{ title }}</h4>
+    <p class="seconday-text">{{ subText }}</p>
+  </div>
+</template>
+
+<style scoped>
+    
+
+    .card {
+        @apply bg-white rounded-sm shadow-md w-80 p-3
+    }
+</style>
