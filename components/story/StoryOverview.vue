496f37064309a2bf4670fbcf9479806a14fd592d
th7mo <dev@th7mo.com>
2023-03-13 17:18:20 +0100

    feat: show progress label in edit Story dialog

--- components/story/StoryOverview.vue
+++ components/story/StoryOverview.vue
@@ -16,11 +16,15 @@
   <BaseDialog @close="emit('close')" capped-width>
     <header>
       <p class="story-key">STORY-{{ story.key }}</p>
-      <BaseCloseButton @close="emit('close')" />
+      <StoryProgressLabel :story="story"/>
+      <BaseCloseButton @close="emit('close')" class="close-button"/>
     </header>
     <StoryCreateInput v-model="story.summary" borderless class="summary" :rows="1" />
 
-    <p class="static-description">Description</p>
+    <ul>
+      <p class="static-description">Description</p>
+    </ul>
+
     <StoryCreateTextArea
       v-model="story.description"
       borderless
@@ -33,11 +37,7 @@
 
 <style scoped lang="scss">
   header {
-    @apply flex justify-between items-center;
-  }
-
-  p.story-key {
-    @apply text-sm;
+    @apply flex items-center gap-7;
   }
 
   .summary {
@@ -45,10 +45,14 @@
   }
 
   p.static-description {
-    @apply font-semibold mb-2 pl-2;
+    @apply font-semibold pl-2 mb-2;
   }
 
   .description {
     @apply max-w-2xl text-gray-700;
+  }
+
+  .close-button {
+    @apply ml-auto;
   }
 </style>
