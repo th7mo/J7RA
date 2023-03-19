f9a922051d4288078f77963eb59414c52625decc
th7mo <dev@th7mo.com>
2023-03-19 11:18:15 +0100

    bump: versions of nuxt and sass

---
+++ components/base/BaseTooltip.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+  defineProps<{
+    text: string;
+  }>();
+
+  const isAnimating = ref(true);
+  setTimeout(() => {
+    isAnimating.value = false;
+  }, 500);
+</script>
+
+<template>
+  <span
+    :class="{ fade: isAnimating }"
+    class="bg-slate-800 rounded px-2 absolute top-10 text-[0.6rem] text-white delay-500"
+    >{{ text }}</span
+  >
+</template>
+
+<style scoped lang="scss">
+  .fade {
+    animation: fade 0.82s;
+  }
+
+  @keyframes fade {
+    100% {
+      transform: translateX(0);
+    }
+
+    20%,
+    80% {
+      transform: translate3d(2px, 0, 0);
+    }
+
+    30%,
+    50%,
+    70% {
+      transform: translate3d(-4px, 0, 0);
+    }
+
+    40%,
+    60% {
+      transform: translate3d(4px, 0, 0);
+    }
+  }
+</style>
--- components/story/list/StoryListItemOptions.vue
+++ components/story/list/StoryListItemOptions.vue
@@ -10,19 +10,30 @@
   function toggleOptionsList() {
     isOptionsListShown.value = !isOptionsListShown.value;
   }
+
+  const isTooltipShown = ref(false);
+
+  function setTooltipVisibleWithTimer() {
+    isTooltipShown.value = true && !isOptionsListShown.value;
+  }
 </script>
 
 <template>
-  <BaseEllipsisButton
-    @click="toggleOptionsList"
-    @blur="isOptionsListShown = false"
-    tabindex="1"
-    v-bind="$attrs"
-  />
+  <li class="relative">
+    <BaseEllipsisButton
+      @click="toggleOptionsList"
+      @blur="isOptionsListShown = false"
+      tabindex="1"
+      v-bind="$attrs"
+      @mouseover="setTooltipVisibleWithTimer"
+      @mouseleave="isTooltipShown = false"
+    />
+    <BaseTooltip text="Edit" class="-left-1" v-if="isTooltipShown"></BaseTooltip>
 
-  <StoryListItemOptionsList
-    v-if="isOptionsListShown"
-    :story="story"
-    class="top-11 right-2 absolute z-50"
-  />
+    <StoryListItemOptionsList
+      v-if="isOptionsListShown"
+      :story="story"
+      class="top-8 right-0 absolute z-50 translate-x-10"
+    />
+  </li>
 </template>
